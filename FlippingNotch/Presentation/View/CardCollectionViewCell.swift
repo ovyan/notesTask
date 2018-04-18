54fcf2ec1a56b95faa2cd5a306c369d3dd915d78
Mike Ovyan <ovyan13@ya.ru>
2018-04-18 20:07:21 +0300

    kokes

--- FlippingNotch/Presentation/View/CardCollectionViewCell.swift
+++ FlippingNotch/Presentation/View/CardCollectionViewCell.swift
@@ -85,7 +85,7 @@ public final class CardCollectionViewCell: UICollectionViewCell {
     // MARK: - Internal
 
     private func updateView(with model: TaskModel) {
-        noteTitleLabel.text = "Some title"
+        //noteTitleLabel.text = "Some title"
         noteTextView.text = model.text
         headerView.backgroundColor = model.isImportant ? .red : .green
     }
